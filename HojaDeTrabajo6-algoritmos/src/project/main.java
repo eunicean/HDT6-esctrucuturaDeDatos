e430525f1ce0da455c5867b4e56e191ae7018181
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-03-02 11:48:23 -0600

    progress

--- HojaDeTrabajo6-algoritmos/src/project/main.java
+++ HojaDeTrabajo6-algoritmos/src/project/main.java
@@ -1,10 +1,15 @@
 package project;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.io.*;  
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner teclado = new Scanner(System.in);
+		Warehouse myWarehouse;
 	}
 
 }
