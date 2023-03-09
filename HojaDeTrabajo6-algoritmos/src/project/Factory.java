e3d000d3c528fbc4dd1d39a837a33c5800a8d4c1
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-03-08 23:38:04 -0600

    voy a matar al otro integrante de mi equipo :)

--- HojaDeTrabajo6-algoritmos/src/project/Factory.java
+++ HojaDeTrabajo6-algoritmos/src/project/Factory.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package project;
 
 import java.util.HashMap;
@@ -36,48 +36,5 @@ public class Factory {
         return null;
     }
 
-
     
 }
-=======
-package project;
-
-import java.util.HashMap;
-
-import java.util.TreeMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-public class Factory {
-
-	public static Factory instance;
-	public static Factory getInstance() {
-		if(instance == null) {
-			instance = new Factory();
-		}
-		return instance;
-	}
-	
-    public <E,T> Map <E,T> Getfactory(String hash) {
-
-        if(hash == "hash"){
-
-            return new HashMap();
-        }
-
-        else if(hash == "tree"){
-
-            return new TreeMap();
-        }
-
-        else if(hash == "linked"){
-
-            return new LinkedHashMap();
-        }
-
-        return null;
-    }
-
-
-    
-}
->>>>>>> 8a7179b28531691b4c4f79e2c65ad98082cd6a48
--- HojaDeTrabajo6-algoritmos/src/project/main.java
+++ HojaDeTrabajo6-algoritmos/src/project/main.java
@@ -3,18 +3,7 @@ package project;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
-<<<<<<< HEAD
 import java.io.*;  
-
-public class main {
-
-	public static void main(String[] args) {
-		Scanner teclado = new Scanner(System.in);
-		Warehouse myWarehouse;
-		
-		System.out.println("Seleciones el tipo de Hash a utilizar: \n 1. HashMap \n 2. TreeMap \n 3.Linked HashMap");
-=======
-import java.io.*; 
 
 public class main {
 
@@ -108,8 +97,6 @@ public class main {
 
 		}
 
-		
->>>>>>> 8a7179b28531691b4c4f79e2c65ad98082cd6a48
 	}
 
 }
