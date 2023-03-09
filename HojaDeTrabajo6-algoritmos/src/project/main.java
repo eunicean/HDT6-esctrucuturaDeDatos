972acf5d20204e79f1ebf38414e1bc58d697002b
Pablo Josue Noack Cajbon <noa17596@uvg.edu.gt>
2023-03-08 23:04:42 -0600

    estoy agregando las decisiones, pero me faltan los metodos entonces guardo desde acá

--- HojaDeTrabajo6-algoritmos/src/project/main.java
+++ HojaDeTrabajo6-algoritmos/src/project/main.java
@@ -9,7 +9,14 @@ public class main {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner teclado = new Scanner(System.in);
-		Warehouse myWarehouse = new Warehouse("hash");
+		
+	
+		  
+		System.out.println("Seleciones el tipo de Hash a utilizar: \n 1. HashMap \n 2. TreeMap \n 3.Linked HashMap");
+		String estructura= teclado.nextLine();   
+		Warehouse myWarehouse = new Warehouse(estructura);  
+		Warehouse client = new Warehouse(estructura); 
+		
 		File file = new File("C:\\Users\\pablo\\Desktop\\Estructura de datos\\Git\\Tarea 6\\Continuando\\OWarehouse\\HojaDeTrabajo6-algoritmos\\src\\project\\ListadoProducto.txt");
 		Scanner sc = new Scanner(file);
 	 
@@ -21,23 +28,28 @@ public class main {
 		  valor[1] = valor[1].trim(); 
 		  String[] producto =  {valor[0], valor[1], "0" };    
 		  myWarehouse.addProduct(producto);
+		}
 
-		  //System.out.println(sc.nextLine());
-		  for (String a : valor){
-			System.out.println(a);
+		int pochochu = 0 ; 
 
-		  }
-            
+		while (pochochu == 0){
+			
+			System.out.println("¿Buen día mi buen amigo, que desea hacer el día de hoy?");
+			System.out.println("1. Agregar un producto?");
+			System.out.println("2. Mostrar la categoria de un producto?");
+			System.out.println("3. Mostrar los datos del producto, categoría y la cantidad de cada artículo que tienes en tu colección");
+			System.out.println("4. Mostrar los datos del producto, categoría y la cantidad de cada artículo que tienes en tu colección :3");
+			System.out.println("5. Mostrar el producto y la categoría de todo el inventario.");
+			System.out.println("6. Mostrar el producto y la categoría existentes, ordenadas por tipo.");
+			System.out.println("¿Tons que piensas hacer?");
+
+
+
+
 
 
 
 		}
-		  
-
-
-
-		
-		System.out.println("Seleciones el tipo de Hash a utilizar: \n 1. HashMap \n 2. TreeMap \n 3.Linked HashMap");
 
 		
 	}
