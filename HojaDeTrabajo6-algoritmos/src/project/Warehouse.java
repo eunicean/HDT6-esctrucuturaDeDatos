f7ddfd705e933129aba33b5b0fee21c845ef9c0a
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-03-08 23:28:36 -0600

    idk pt2

--- HojaDeTrabajo6-algoritmos/src/project/Warehouse.java
+++ HojaDeTrabajo6-algoritmos/src/project/Warehouse.java
@@ -30,16 +30,60 @@ public class Warehouse {
 	public String getCategoryOfProduct(String productName) {//2
 		String r = "";
 		
+		ArrayList<String> myKeys = new ArrayList<String>(myMap.keySet());
+		for(int i=0; i<myKeys.size();i++) {
+			Product temp = new Product(productName,0);
+			if(myMap.get(myKeys.get(i)).contains(temp)) {
+				r += myKeys.get(i) + " ";
+			}
+		}
+		
 		return r;
 	}
 	public String getDataProduct(String productName) {//3
 		String r = "";
+		ArrayList<String> myKeys = new ArrayList<String>(myMap.keySet());
+
+		Product temp = new Product(productName);
+		for(int i=0; i<myKeys.size();i++) {
+			if(myMap.get(myKeys.get(i)).get(0).getCuantity() == 0) { //inventory
+				ArrayList<Product> ProductsLsit = new ArrayList<Product>(myMap.get(myKeys.get(i)));
+				for(int j=0; j<ProductsLsit.size();j++) {
+					if(ProductsLsit.get(j).getName().equals(temp.getName())) {
+						r = myKeys.get(i) + " \n " + ProductsLsit.get(j).toStringInventory();
+					}
+				}
+			}
+			else { //cart
+				ArrayList<Product> ProductsLsit = new ArrayList<Product>(myMap.get(myKeys.get(i)));
+				for(int j=0; j<ProductsLsit.size();j++) {
+					if(ProductsLsit.get(j).getName().equals(temp.getName())) {
+						r = myKeys.get(i) + " \n " + ProductsLsit.get(j).toStringCart();
+					}
+				}
+			}
+		}
 		
 		return r;
 	}
-	public String getDataProductSorted(String[] productsName) {//4
+	public String getDataProductSorted() {//4
 		String r = "";
-		
+		ArrayList<String> myKeys = new ArrayList<String>(myMap.keySet());
+		for(int i=0; i<myKeys.size();i++) {
+			r += myKeys.get(i) + " \n";
+			if(myMap.get(myKeys.get(i)).get(0).getCuantity() == 0) { //inventory
+				ArrayList<Product> ProductsLsit = new ArrayList<Product>(myMap.get(myKeys.get(i)));
+				for(int j=0; j<ProductsLsit.size();j++) {
+					r += ProductsLsit.get(j).toStringInventory() + "\n";
+				}
+			}
+			else { //cart
+				ArrayList<Product> ProductsLsit = new ArrayList<Product>(myMap.get(myKeys.get(i)));
+				for(int j=0; j<ProductsLsit.size();j++) {
+					r += ProductsLsit.get(j).toStringCart() + "\n";
+				}
+			}
+		}
 		return r;
 	}
 	public String getAllData() {
